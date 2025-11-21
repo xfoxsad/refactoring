c2fc517b3dbf2248982a2ccd8b54c5700bd1cc96
xfoxsad <x.hu.shuai@gmail.com>
2025-11-21 16:09:49 -0500

    Task 1.1 fix checkstyle issues

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,23 @@
 package theater;
 
+/**
+ * Represents a play with a name and type (e.g., tragedy or comedy).
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
